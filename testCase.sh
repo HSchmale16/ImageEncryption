c894ae8301dac0a488ee0ba3e7df812891c1680c
Henry J Schmale <hschmale16@gmail.com>
2015-02-28 16:18:43 -0500

    Added a test case script
    The test case script encrypts a given file with a given key then
    decrypts that file, and diffs it against the original

---
+++ testCase.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# @author Henry J Schmale
+# @date February 28, 2015
+# Test Case for Image Encrypter
+
+EXE=./ImgCrypt
+KEY="QWERTY7991"
+ORIGNAL="main.cpp"
+IMG_OUT="$ORIGNAL.bmp"
+TEXT_OUT="$IMG_OUT.txt"
+
+make # Build the program
+
+# encryption test
+$EXE e $ORIGNAL $IMG_OUT $KEY
+
+# decryption test
+$EXE d $IMG_OUT $TEXT_OUT $KEY
+
+diff $ORIGNAL $IMG_OUT
