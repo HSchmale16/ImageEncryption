9226cda7d9c67602c5cdfc85c5394f25cca9afac
Henry J Schmale <hschmale16@gmail.com>
2015-03-05 15:42:55 -0500

    Script Changes
    Started to modify test script for project changes
    
    project changes are splitting crypto and steno into 2 seperate executables

--- src/testCase.sh
+++ src/testCase.sh
@@ -3,24 +3,18 @@
 # @date February 28, 2015
 # Test Script for Image Encrypter Program by me
 
+# NOTE: THE SUCCESS/FAIL BRACKET STARTS at col50
 
 # TEST VARS
-KEY="QWERTY7991"           # Key to use for encryption
-ORIGNAL="main.cpp"         # file to use as test data
+key="QWERTY7991"              # Key to use for encryption
+testdata="main.cpp"           # file to use as test data
 
 # -------------------------------------
-# Begin Primary Script
+# Private Variables
 # -------------------------------------
-# Clean up, this should only happen on pass
-function CleanUp {
-    make clean > /dev/null
-    if [ -e $IMG_OUT ] ; then rm $IMG_OUT ; fi
-    if [ -e $TEXT_OUT ] ; then rm $TEXT_OUT ; fi
-}
-# File Vars
-EXE=./ImgCrypt             # The executable
-IMG_OUT="$ORIGNAL.bmp"     # First output
-TEXT_OUT="$IMG_OUT.txt"    # Final output
+cryptTxtOut="$testdata.crypt"      # output for crypto test
+
+
 
 # Pass or Fail Colorize Vars
 PASS='\033[0;32m'
@@ -28,47 +22,35 @@ FAIL='\033[0;31m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
-NDEV=/dev/null             # Null Device
+NDEV=/dev/null                # Null Device
 
-CleanUp
+# -------------------------------------
+# Script Functions
+# -------------------------------------
+
+# -------------------------------------
+# Begin Primary Script
+# -------------------------------------
 echo "ImgCrypt Test Cases"
-echo -e "Test Data: ${BLUE}$ORIGNAL${NC}"
-echo -e "Test Key:  ${BLUE}$KEY${NC}"
+echo -e "Test Data: ${BLUE}$original${NC}"
+echo -e "Test Key:  ${BLUE}$key${NC}"
 
 # -------------------------------------
 # Build The Program
-echo -n "Building Program                        ["
+echo -n "Program Compiled Successfully           ["
 make >$NDEV
 if [ $? -eq 0 ] ; then
     echo -e "${PASS}PASS${NC}]"
 else
     echo -e "${FAIL}FAIL${NC}]"
+    exit
 fi
 
-# -------------------------------------
-# encryption test
-echo -n "Encrypting Some data                    ["
-$EXE e $ORIGNAL $IMG_OUT $KEY >$NDEV
-if [ -s $IMG_OUT ] ; then
-    echo -e "${PASS}PASS${NC}]"
-else
-    echo -e "${FAIL}FAIL${NC}]"
-fi
- 
-# -------------------------------------
-# decryption test
-echo -n "Decrypting some data                    ["
-$EXE d $IMG_OUT $TEXT_OUT $KEY >$NDEV
-if [ -s $TEXT_OUT ] ; then
-    echo -e "${PASS}PASS${NC}]"
-else
-    echo -e "${FAIL}FAIL${NC}]"
-fi
 
 # -------------------------------------
 # Data Intergretity Test
 echo -n "Now Performing Data Intergretity Test   ["
-cmp -s $ORIGNAL $TEXT_OUT >/dev/null
+cmp -s $testdata $text_out >/dev/null
 if [ $? -eq 0 ]; then
     echo -e "${PASS}PASS${NC}]"
     echo "Running Clean up, as there is a bunch of junk files now"
