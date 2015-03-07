e7bddac776ce3421b6f0aad8b27226c6d5dc29d1
Henry J Schmale <hschmale16@gmail.com>
2015-03-06 23:38:40 -0500

    Man Page
    Wrote a little bit of a man page for the Stenography sub main demo.
    Explains usage of the args and what not

--- src/mainSteno.cpp
+++ src/mainSteno.cpp
@@ -1,10 +1,31 @@
-/** Image Encrypter
+/** Stenography
  * \author Henry J Schmale
  * \date February 25, 2015
  * \file mainSteno.cpp
  *
  * This file contains the main function to demonstrate
  * the stenography system.
+ *
+ * USAGE:
+ * argv[0] <h|r|a> <input file> <output file> [<x> <y> <width> <height>]
+ *
+ * 1. <h|r|a>
+ *    * h - hide, data inside of an image
+ *    * r - recover, data inside of an image
+ *    * a - average, determine pixel value averages in region specified
+ * 
+ * 2. <input file>
+ *    
+ *    File to read from
+ *
+ *    if (r) is specified then it should be an image file else a txt file
+ *    
+ *    \note file extensions are not currently verified.
+ * 
+ * 3. <output file>
+ *    
+ *    File to output to
+ *
  */
 
 #include "steno.h"
