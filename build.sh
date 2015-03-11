3c6f52fa6f8ce943bb8816b3b0907e0fb595dda9
Henry J Schmale <hschmale16@gmail.com>
2015-03-10 21:25:55 -0400

    Expand Build Script

--- build.sh
+++ build.sh
@@ -12,22 +12,30 @@ latexEng=pdflatex
 NC='\033[0m'                           # No Color
 PASS='\033[0;32m'                      # Green, passed test
 FAIL='\033[0;31m'                      # Red, failed test
+NULL='/dev/null'                       # Null Device
 
-# Build the src code & test the exe on
-# encryption and decryption
-cd $srcDir
-echo $(pwd)
-./testCase.sh                          # Builds and tests the src code
-cd $projectDir                         # Return to projectDir
+if [ $# -eq 1 ] ; then                 # If they're no args then build
+    # Build the src code & test the exe on
+    # encryption and decryption
+    cd $srcDir
+    echo $(pwd)
+    ./testCase.sh                      # Builds and tests the src code
+    cd $projectDir                     # Return to projectDir
 
-# Article Builder
-cd $articleDir
-echo -n -e 'Making Article                          ['
-$latexEng Encryption.tex > /dev/null   # Silence a noisy command
-bibtex *.aux > /dev/null
-$latexEng Encryption.tex > /dev/null
-if [ $? -eq 0 ] ; then
-    echo -e "${PASS}PASS${NC}]"
+    # Article Builder
+    cd $articleDir
+    echo -n -e 'Making Article                          ['
+    $latexEng Encryption.tex > $NULL   # Silence a noisy command
+    bibtex *.aux > /dev/null
+    $latexEng Encryption.tex > $NULL
+    if [ $? -eq 0 ] ; then
+        echo -e "${PASS}PASS${NC}]"
+    else
+        echo -e "${FAIL}FAIL${NC}]"
+    fi
 else
-    echo -e "${FAIL}FAIL${NC}]"
+    echo 'An arg has been detected, this triggers a clean build'
+    echo -n -e "Remove Built Article                    ["
+    if [ -e article/*.pdf ] ; then rm article/*.pdf ; fi
+    echo -e "${PASS}PASS${NC}]"
 fi
